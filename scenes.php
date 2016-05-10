ec2b40833bd12202f89d4e2c1d62c74e92d1a505
prosenkranz <pascal@rosi-online.com>
2016-05-10 15:59:04 +0200

    Create and update a scenes.json file in scenesDir
    This file could be used by the game to load a list of available
    scenes without using php. The scenes.json file is automatically
    updated everytime the editor queues the list of scenes (that means
    at least everytime you reload the editor).
    
    You should not need to manage the scenes.json file on your own!
    
    References #9

--- scenes.php
+++ scenes.php
@@ -6,33 +6,6 @@
 //
 //------------------------------------------------------------------------------
 
-function list_scenes($dir)
-{
-	$files = scandir($dir);
-	echo "[";
-	$end = end($files);
-	foreach ($files as $file)
-	{
-		$i = strpos($file, '.json');
-		if (is_file($dir . $file) && $i !== false)
-			echo "\"" . substr($file, 0, $i) . "\"" . ($file != $end ? ',' : '');
-	}
-	echo "]";
-}
-
-function load_scene($file)
-{
-	echo file_get_contents($file);
-}
-
-function save_scene($file, $json)
-{
-	file_put_contents($file, $json);
-	echo "OK";
-}
-
-//------------------------------------------------------------------------------
-
 // Load config
 $configJSON = file_get_contents("config.json");
 $config = json_decode($configJSON, true);
@@ -43,12 +16,73 @@ if (array_key_exists('scenesDir', $config))
 
 $dir = rtrim($dir, '/\\') . '/';
 
+//------------------------------------------------------------------------------
+
+function get_scenes_json()
+{
+	global $dir;
+	$scenes = Array();
+	$files = scandir($dir);
+	$end = end($files);
+	foreach ($files as $file)
+	{
+		$i = strpos($file, '.json');
+		if (is_file($dir . $file) && $i !== false && $file != 'scenes.json')
+			$scenes[] = substr($file, 0, $i);
+	}
+
+	return json_encode($scenes);
+}
+
+function update_scenes_json()
+{
+	global $dir;
+	$json = get_scenes_json();
+	file_put_contents($dir . 'scenes.json', $json);
+	return $json;
+}
+
+//------------------------------------------------------------------------------
+
+function list_scenes()
+{
+	echo update_scenes_json();
+}
+
+function load_scene($name)
+{
+	global $dir;
+	echo file_get_contents($dir . $name . '.json');
+}
+
+function save_scene($name, $jsonData)
+{
+	global $dir;
+	file_put_contents($dir . $name . '.json', $jsonData);
+	update_scenes_json();
+	if (!file_exists($dir . $name . '.js'))
+	{
+		$js = "var " . $name . " = function(phaserGame) {\n"
+			. "\tCScene.call(this);\n"
+			. "\n"
+			. "\tthis.onLoad = function() {\n"
+			. "\t}\n"
+			. "\n"
+			. "\tthis.load('scenes/" . $name . ".json');\n"
+			. "}\n";
+		file_put_contents($dir . $name . '.js', $js);
+	}
+
+	echo "OK";
+}
+
+//------------------------------------------------------------------------------
 
 // Handle request
 if (!empty($_POST['save']) && !empty($_POST['json']))
-	save_scene($dir . $_POST['save'] . '.json', $_POST['json']);
+	save_scene($_POST['save'], $_POST['json']);
 else if (!empty($_GET['name']))
-	load_scene($dir . $_GET['name'] . '.json');
+	load_scene($_GET['name']);
 else
-	list_scenes($dir);
+	list_scenes();
 ?>
